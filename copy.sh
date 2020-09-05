e684adb1c8e8f133fd79c703e9823b98c4446b89
Giulio Russo <me@giuuliorusso.me>
2020-09-05 04:06:55 +0200

    Copy VS Code files with copy.sh

--- copy.sh
+++ copy.sh
@@ -1,17 +1,35 @@
 #!/usr/bin/env bash
 
-function doIt() {
+function copy() {
+  # Copy home files
   rsync --exclude ".DS_Store" \
     -avh --no-perms ./home/ ~;
+
+  # Copy vscode files
+  case $os in
+    Darwin ) p="$HOME/Library/Application Support/Code/User/";;
+    * ) p="$HOME/.config/Code/User/";;
+  esac
+
+  mkdir -p "$p";
+
+  rsync --exclude ".DS_Store" \
+    -avh --no-perms ./vscode/ "$p";
+
+  unset p;
 }
 
 cd "$(dirname "$BASH_SOURCE")";
 git pull origin master &> /dev/null;
 
+os=$(uname);
+echo "OS: $os";
+
 read -p "This may overwrite existing files in your home directory. Are you sure? (y/N) " answer;
 case $answer in
-  Yes | yes | Y | y ) echo -e "Ok :)\n"; doIt;;
+  Yes | yes | Y | y ) echo -e "Ok :)\n"; copy;;
   * ) echo "Bye :(";;
 esac
 
-unset doIt;
+unset copy;
+unset os;
